f17882021e51d9cfbc12c89d6024c1ec7ecc6792
Kiva Aleksey <adolfp@mail.ru>
2021-11-18 11:47:46 +0700

    Init commit

---
+++ src/main/java/ru/netology/cloudservisebackend/entity/User.java
@@ -0,0 +1,41 @@
+package ru.netology.cloudservisebackend.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    private Long id;
+    private String name;
+    private String username;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o))
+            return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/test/java/ru/netology/cloudservisebackend/CloudServiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudservisebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
