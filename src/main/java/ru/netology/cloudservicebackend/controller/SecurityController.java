9997f7b3d5a95d1e8bbc0b743c14d37e44dc429c
Kiva Aleksey <adolfp@mail.ru>
2021-11-21 12:31:07 +0700

    Edit SecurityController.java

--- src/main/java/ru/netology/cloudservicebackend/controller/SecurityController.java
+++ src/main/java/ru/netology/cloudservicebackend/controller/SecurityController.java
@@ -3,13 +3,10 @@ package ru.netology.cloudservicebackend.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 public class SecurityController {
 
     @PostMapping("/login")
-    public HttpServletResponse postLogin(HttpServletResponse response) {
-        return response;
+    public void postLogin() {
     }
 }
